a8b9a858e1a23649f3bf107213b595479c490089
Zachary Martinez <36873191+martinez-zacharya@users.noreply.github.com>
2023-03-31 11:48:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@
 
 from setuptools import setup
 
-setup(py_modules = ['inference', 'utils.diffusion_utils', 'utils.inference_utils', 'utils.sampling', 'utils.utils', 'utils.visualise', 'datasets.process_mols'])
+setup(py_modules = ['inference', 'dockutils.diffusion_utils', 'dockutils.inference_utils', 'dockutils.sampling', 'dockutils.utils', 'dockutils.visualise', 'dockdatasets.process_mols'])
